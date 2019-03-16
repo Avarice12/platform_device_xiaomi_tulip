e7f0f0bdc04ab8b633b7da5046ee9b746c8c9a7e
Hazari PraveenKumar <phazari@codeaurora.org>
2019-03-16 15:51:19 +0100

    tulip: Disable VSync for CPU rendered apps
    Choreographer VSync has some performance impact for CPU rendered apps
    as the app has to wait for VSync to start a new frame draw. When the
    frame draw time exceeds one VSync, this has a direct impact on the FPS.
    "debug.cpurend.vsync" property controls the VSync for CPU rendered apps.
    
    Disable VSync for CPU rendered app, by setting this property to false.
    
    Change-Id: I93cfa7c588f1ab80803940c8059073075fd49506

--- properties.mk
+++ properties.mk
@@ -153,6 +153,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=9000 \
     ro.surface_flinger.wcg_composition_dataspace=143261696
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.cpurend.vsync=false
+
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
