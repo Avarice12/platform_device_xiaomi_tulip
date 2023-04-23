bittencourtjulival/tulip-ci

tulip: Add display config file
 * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
 * DisplayDeviceConfig: requesting nits when no mapping exists.

Signed-off-by: orges <me@orgesified.ml>
Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf
Signed-off-by: clarencelol <clarencekuiek@icloud.com>

device.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Display calibration
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/display/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_boe_ft8719_fhdplus_video_mode_dsi_panel.xml \

Avarice12/platform_device_xiaomi_tulip

Signed-off-by: pix106 <sbordenave@gmail.com>
Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

device.mk
@@ -186,6 +186,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/display/qdcm_calib_data_tianma_nt36672a_fhdplus_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_tianma_nt36672a_fhdplus_video_mode_dsi_panel.xml \
     $(DEVICE_PATH)/configs/display/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \